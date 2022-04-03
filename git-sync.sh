ac0b8d6346a34a41e8aaa3822c2e9f9649b8d159
sig-alanyan <alanyan@synopsys.com>
2022-04-03 17:49:59 -0600

    debug

--- git-sync.sh
+++ git-sync.sh
@@ -53,8 +53,6 @@ git config --global user.name "sig-alanyan"
 
 # git checkout -b ${SOURCE_BRANCH} source/${SOURCE_BRANCH}
 git pull destination "${DESTINATION_BRANCH}" --rebase
-echo 'perform here 1'
 git push ${SOURCE_BRANCH}
-echo 'perform here 2'
-git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
 #git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}"
+git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}"
