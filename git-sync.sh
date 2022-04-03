2d46ef24c21c98d7cd253fe1888df5fd3131f7b0
sig-alanyan <alanyan@synopsys.com>
2022-04-03 14:36:46 -0600

    1. git rebase pull before push changes to destionation 2. git push force too dangerous.

--- git-sync.sh
+++ git-sync.sh
@@ -48,4 +48,6 @@ if [[ -n "$DESTINATION_SSH_PRIVATE_KEY" ]]; then
   git config --local core.sshCommand "/usr/bin/ssh -i ~/.ssh/dst_rsa"
 fi
 
-git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
+git pull destination "${DESTINATION_BRANCH}:${SOURCE_BRANCH}" --rebase
+#git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
+git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}"
