3be5ed2ee021dfd8112ede4445b970aa372d138d
sig-alanyan <alanyan@synopsys.com>
2022-04-03 15:13:57 -0600

    debug

--- git-sync.sh
+++ git-sync.sh
@@ -48,6 +48,6 @@ if [[ -n "$DESTINATION_SSH_PRIVATE_KEY" ]]; then
   git config --local core.sshCommand "/usr/bin/ssh -i ~/.ssh/dst_rsa"
 fi
 
-git pull destination "${DESTINATION_BRANCH}:${SOURCE_BRANCH}" --rebase
+git pull destination "${DESTINATION_BRANCH}" --rebase
 #git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
 git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}"
